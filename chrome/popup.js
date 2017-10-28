00c9e783b7cb98fb0ed2f907d4774da916b51f0e
Greg Rosich <pyroscout14@gmail.com>
2017-10-28 12:34:19 -0600

    gerg nope

--- chrome/popup.js
+++ chrome/popup.js
@@ -8,161 +8,73 @@
  * @param {function(string)} callback called when the URL of the current tab
  *   is found.
  */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
 
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
-
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-function myMove() {
-    var elem = document.getElementById("animate"); 
-    var pos = 0;
-    var id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++; 
-            elem.style.top = pos + 'px'; 
-            elem.style.left = pos + 'px'; 
+function httpPostAsync(body, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+          document.getElementById("animate").innerHTML = xmlHttp.responseText;
+          // callback(xmlHttp.responseText);
         }
     }
+    xmlHttp.open("POST", "https://www.googleapis.com/prediction/v1.6/projects/camel-paper/trainedmodels/final-camel/predict?key=AIzaSyAdrK88leKmMObW3J9UK0xBdAYQ1Zzxuco", true); // true for asynchronous 
+    xmlHttp.send(body);
 }
 
-function test() {
-  var title = 'checking for impostors';
-  var titleRandom = '';
-  var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
-  var i = 0
-  var id = setInterval(generateRandomTitle, 70);
-  for( var i=0; i < title.length+1; i++ ) {
-    titleRandom = title.substr(0, i);
-    for( var j=i; j < title.length; j++ ) { 
-      titleRandom += possible.charAt(Math.floor(Math.random() * possible.length)); 
-    }
-    generateRandomTitle(i, titleRandom);
-    titleRandom = '';
-  }
-}
 
-function generateRandomTitle(i, titleRandom) {
-  setTimeout( function() {
-    var elem = document.getElementById("animate");
-    elem.innerHTML = titleRandom;
-  }, i*70 );
-}
-
-function titleAnimation(repeat) {
-  var title = 'checking for impostors';
-  var titleRandom = '';
-  var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
-  var i = 0;
-  var id = setInterval(frame, 100);
-  function frame() {
-    titleRandom = title.substr(0, i);
-    for( var j=i; j < title.length; j++ ) { 
-      titleRandom += possible.charAt(Math.floor(Math.random() * possible.length)); 
-    }
-    document.getElementById("animate").innerHTML = titleRandom;
-    titleRandom = '';
-    if(i === title.length) {
-      clearInterval(id);
-      if(repeat) {
-        setTimeout(titleAnimation, 2000);
+var request = gapi.client.request({
+      'method': 'POST',
+      'path': 'https://www.googleapis.com/prediction/v1.6/projects/camel-paper/trainedmodels/final-camel/predict',
+      'params': {
+        'input': {
+          'csvInstance': [ ]
+        }
       }
-    }
-    i++;
-  }
-}
+    });
 
+function start() {
+  // Initializes the client with the API key and the Translate API.
+  gapi.client.init({
+    'apiKey': 'AIzaSyAdrK88leKmMObW3J9UK0xBdAYQ1Zzxuco',
+    'clientId': '2e7b991052aa2a2cbfa22940e7848482806f47a2'
+  }).then(function() {
+    // Executes an API request, and returns a Promise.
+    // The method name `language.translations.list` comes from the API discovery.
+    return request;
+  }).then(function(response) {
+    console.log(response.result.data);
+  }, function(reason) {
+    console.log('Error: ' + reason.result.error.message);
+  });
+};
+
+function queryTrainedModel(query) {
+  // Replace this value with the project number listed in the Google
+  // APIs Console project.
+  var projectNumber = 'XXXXXXXX';
+  var id = 'final-camel';
+
+  var prediction = Prediction.Trainedmodels.predict(
+      {
+        input:
+          {
+            csvInstance: [query]
+          }
+      },
+      projectNumber,
+      id);
+  // Logs Language: Spanish.
+  Logger.log('Language: ' + prediction.outputLabel);
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl((url) => {
-    var repeat = true;
-    titleAnimation(repeat);
-    repeat = false;
+  
+
+
+
+    
   });
 });
 
